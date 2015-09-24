ff6ca630f3f0850fbb868b83c9e630ce30a37e17
gcielniak <gcielniak@lincoln.ac.uk>
2015-09-24 22:13:16 +0100

    First complete prototype with logging

--- app/src/main/java/com/example/gcielniak/tangoscanner/MainActivity.java
+++ app/src/main/java/com/example/gcielniak/tangoscanner/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.gcielniak.tangoscanner;
 
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -24,13 +27,53 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.SystemClock;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+enum DeviceType
+{
+    BT_BEACON,
+    WIFI_AP
+};
+
+class Scan
+{
+    public DeviceType device_type;
+    public String name;
+    public String mac_address;
+    public long timestamp;
+    public double value;
+
+    public boolean equals (Object o)
+    {
+        if (o instanceof Scan)
+            return this.mac_address.equals(((Scan)o).mac_address);
+
+        return false;
+    }
+
+    @Override
+    public String toString() {
+
+        String _name = new String();
+        if (name != null)
+            _name = name;
+
+        if (device_type == DeviceType.BT_BEACON)
+            return new String("BT: t=" + timestamp + " n=\"" + _name + "\" a=" + mac_address + " v=" + value);
+        else
+            return new String("WF: t=" + timestamp + " n=\"" + _name + "\" a=" + mac_address + " v=" + value);
+    }
+};
 
 /**
  * Main Activity for the Tango Java Quickstart. Demonstrates establishing a
@@ -39,6 +82,8 @@ import android.widget.Toast;
  * {@link TangoConfig}.
  */
 public class MainActivity extends Activity {
+    private List<Scan> current_scan;
+
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final String sTranslationFormat = "Translation: %.2f, %.2f, %.2f";
     private static final String sRotationFormat = "Rotation: %.2f, %.2f, %.2f, %.2f";
@@ -53,10 +98,11 @@ public class MainActivity extends Activity {
 
     WifiManager wifi;
     WifiScanReceiver wifiScanReceiver;
-    String wifis[];
     TangoPoseData current_pose;
     BLeScanCallback bLEScanCallback;
     BluetoothAdapter mBluetoothAdapter;
+    File log_file;
+    FileWriter log_file_writer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,19 +112,30 @@ public class MainActivity extends Activity {
         mTranslationTextView = (TextView) findViewById(R.id.translation_text_view);
         mRotationTextView = (TextView) findViewById(R.id.rotation_text_view);
 
-        // Instantiate Tango client
-        mTango = new Tango(this);
+        current_scan = new ArrayList<Scan>();
 
-        // Set up Tango configuration for motion tracking
-        // If you want to use other APIs, add more appropriate to the config
-        // like: mConfig.putBoolean(TangoConfig.KEY_BOOLEAN_DEPTH, true)
-        mConfig = mTango.getConfig(TangoConfig.CONFIG_TYPE_CURRENT);
-        mConfig.putBoolean(TangoConfig.KEY_BOOLEAN_MOTIONTRACKING, true);
+        // Instantiate Tango client
+        try
+        {
+            mTango = new Tango(this);
+            // Set up Tango configuration for motion tracking
+            // If you want to use other APIs, add more appropriate to the config
+            // like: mConfig.putBoolean(TangoConfig.KEY_BOOLEAN_DEPTH, true)
+            mConfig = mTango.getConfig(TangoConfig.CONFIG_TYPE_CURRENT);
+            mConfig.putBoolean(TangoConfig.KEY_BOOLEAN_MOTIONTRACKING, true);
+        }
+        catch (Throwable exc)
+        {
+            Log.i(TAG,"Could not find the Tango");
+        }
 
         //WIFI stuff
         wifi=(WifiManager)getSystemService(Context.WIFI_SERVICE);
-        wifiScanReceiver = new WifiScanReceiver();
-        wifi.startScan();
+        if (wifi != null)
+        {
+            wifiScanReceiver = new WifiScanReceiver();
+            wifi.startScan();
+        }
 
         //BT stuff
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -94,17 +151,26 @@ public class MainActivity extends Activity {
         // the app
         // is brought to the foreground.
 
-        registerReceiver(wifiScanReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        if (wifiScanReceiver != null)
+            registerReceiver(wifiScanReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
 
-        if (mBluetoothAdapter != null) {
+        if (mBluetoothAdapter != null)
             mBluetoothAdapter.startLeScan(bLEScanCallback);
-        }
 
-        if (!mIsTangoServiceConnected) {
+        if ((mTango != null) &&!mIsTangoServiceConnected) {
             startActivityForResult(
                     Tango.getRequestPermissionIntent(Tango.PERMISSIONTYPE_MOTION_TRACKING),
                     Tango.TANGO_INTENT_ACTIVITYCODE);
         }
+
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd'T'HHmmss.ssss");
+            Date current_date = new Date();
+            log_file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "wifi_bt_log_" + sdf.format(current_date) + ".txt");
+            log_file_writer = new FileWriter(log_file, false);
+            log_file_writer.write(sdf.format(current_date) + "=" + current_date.getTime()*1000);
+        }
+        catch (IOException exc) { Log.i("TAG","Error opening the file: " + log_file.getAbsolutePath()); }
     }
 
     @Override
@@ -113,17 +179,37 @@ public class MainActivity extends Activity {
         // When the app is pushed to the background, unlock the Tango
         // configuration and disconnect
         // from the service so that other apps will behave properly.
-        unregisterReceiver(wifiScanReceiver);
-        if (mBluetoothAdapter != null) {
+        if (wifiScanReceiver != null)
+            unregisterReceiver(wifiScanReceiver);
+
+        if (mBluetoothAdapter != null)
             mBluetoothAdapter.stopLeScan(bLEScanCallback);
+
+        if (mTango != null)
+        {
+            try {
+                mTango.disconnect();
+                mIsTangoServiceConnected = false;
+            }
+            catch (TangoErrorException e) {
+                Toast.makeText(getApplicationContext(), "Tango Error!",
+                        Toast.LENGTH_SHORT).show();
+            }
         }
 
-        try {
-            mTango.disconnect();
-            mIsTangoServiceConnected = false;
-        } catch (TangoErrorException e) {
-            Toast.makeText(getApplicationContext(), "Tango Error!",
-                    Toast.LENGTH_SHORT).show();
+        if (log_file_writer != null)
+        {
+            try { log_file_writer.close(); }
+            catch (IOException exc) { Log.i("TAG", "Error opening the file: " + log_file.getAbsolutePath()); }
+
+            MediaScannerConnection.scanFile(MainActivity.this,
+                    new String[]{log_file.getAbsolutePath()}, null,
+                    new MediaScannerConnection.OnScanCompletedListener() {
+
+                        public void onScanCompleted(String path, Uri uri) {
+                            Log.i("TAG", "Finished scanning " + path);
+                        }
+                    });
         }
     }
 
@@ -229,10 +315,6 @@ public class MainActivity extends Activity {
     private class WifiScanReceiver extends BroadcastReceiver {
         public void onReceive(Context c, Intent intent) {
             List<ScanResult> wifiScanList = wifi.getScanResults();
-            wifis = new String[wifiScanList.size()];
-
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd'T'HHmmss.ss");
-            Log.i("MainActivity", sdf.format(new Date()));
 
             if (current_pose != null)
             {
@@ -251,10 +333,30 @@ public class MainActivity extends Activity {
             }
 
             for(int i = 0; i < wifiScanList.size(); i++){
-//                wifis[i] = ((wifiScanList.get(i)).toString());
                 ScanResult result = wifiScanList.get(i);
-                wifis[i] = "WIFI: " + result.SSID + " " + result.timestamp + " " + result.level;
-                Log.i("MainActivity", wifis[i]);
+
+                Scan scan = new Scan();
+                scan.device_type = DeviceType.WIFI_AP;
+                scan.mac_address = result.BSSID.toUpperCase();
+                scan.name = result.SSID;
+                scan.timestamp = result.timestamp;
+                scan.value = (double)result.level;
+
+                int indx = current_scan.indexOf(scan);
+                if (indx != -1) {
+                    if (current_scan.get(indx).timestamp == scan.timestamp)
+                        continue;
+                    current_scan.remove(indx);
+                }
+                current_scan.add(scan);
+
+                Log.i(TAG, scan.toString());
+
+                if (log_file_writer != null)
+                {
+                    try { log_file_writer.write(scan + "\n"); }
+                    catch(IOException exc) { Log.i(TAG, "Error writing to file."); }
+                }
             }
             wifi.startScan();
         }
@@ -263,7 +365,28 @@ public class MainActivity extends Activity {
     private class BLeScanCallback implements BluetoothAdapter.LeScanCallback {
         @Override
         public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
-            Log.i(TAG, "BT: " + device.getAddress() + ", RSSI: " + rssi + "\n");
+            if (device.getType() != BluetoothDevice.DEVICE_TYPE_LE)
+                Log.i(TAG,"Danger!");
+
+            Scan scan = new Scan();
+            scan.device_type = DeviceType.BT_BEACON;
+            scan.mac_address = device.getAddress();
+            scan.name = device.getName();
+            scan.timestamp = SystemClock.elapsedRealtimeNanos()/1000;
+            scan.value = (double)rssi;
+
+            int indx = current_scan.indexOf(scan);
+            if (indx != -1) {
+                current_scan.remove(indx);
+            }
+            current_scan.add(scan);
+
+            Log.i(TAG, scan.toString());
+            if (log_file_writer != null)
+            {
+                try { log_file_writer.write(scan + "\n"); }
+                catch(IOException exc) { Log.i(TAG, "Error writing to file."); }
+            }
         }
     }
 }
